ec8a0ce68203c66857d21ffd3b999a50937c60a2
kozzak2610 <kozzak2610@gmail.com>
2018-10-18 17:34:05 +0300

    homework

--- grebinichenko/three/script.js
+++ grebinichenko/three/script.js
@@ -18,3 +18,25 @@ var pizza = {
     }
 /*alert(pizza.makePizza());
 alert(pizza.packPizza())*/
+var ar=[];
+
+function sum() { // суммирует аргументы: sum(1,2,3) = 6
+    return [].reduce.call(arguments, function(a, b) {
+      return a + b;
+    });
+  }
+  
+  function mul() { // перемножает аргументы: mul(2,3,4) = 24
+    return [].reduce.call(arguments, function(a, b) {
+      return a * b;
+    });
+  }
+  
+ function applyAll(){
+     for (var i = 1; i < arguments.length; i++) {
+         ar.push(arguments[i]);
+         
+      }
+      return arguments[0].apply(null, ar);
+    }
+    console.log(applyAll(sum, 10, 2000, 250));
