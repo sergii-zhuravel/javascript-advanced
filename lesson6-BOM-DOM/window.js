4fd98152ec4ba6e08112bf48c42f7561e30374ad
Sergey <2285124+sergii-zhuravel@users.noreply.github.com>
2018-10-20 08:37:02 +0300

    Updated lessons

---
+++ lesson6-BOM-DOM/window.js
@@ -0,0 +1,10 @@
+
+// window - global object in Browser Environment
+// and contains methods to manage browser window, open new windows, for example:
+//  window.open('https://github.com/sergii-zhuravel/javascript-advanced')
+
+//  document.body.style.background = 'green'
+
+//  BOM - Browser Object Model
+//  DOM - Document Object Model
+
