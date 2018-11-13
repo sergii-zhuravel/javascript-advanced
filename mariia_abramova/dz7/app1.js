f98b0e2649e017a07ce3f265db45ff43a9b985e0
maab2198 <maab2198@gmail.com>
2018-11-13 22:24:31 +0200

    dz7 finish

--- mariia_abramova/dz7/app1.js
+++ mariia_abramova/dz7/app1.js
@@ -22,31 +22,6 @@
 var todo = document.getElementById("todo-input").value;
 var li = document.getElementsByTagName("li")[0];
 var clone = li.cloneNode(true);
-clone.firstChild = "New text";
+clone.textContent = "New text";
 
-// ------------------------
-// Задача 7.2
-// ------------------------
-// Дано:
-// HTML страница в которой есть только такой код
-
-// <div id="test" style="height: 50px;background-color: lightblue;">Test Div</div>
-// <p>Расчетные стили для test div: <br><span id="output"></span></p>
-
-// <script>
-// function myFunction(){
-//     var elem = document.getElementById("test");
-//     var txt;
-//     cssObj = window.getComputedStyle(elem)
-
-//     for (i = 0; i < cssObj.length; i++) { 
-//         cssObjProp = cssObj.item(i)
-//         txt += cssObjProp + " = " + cssObj.getPropertyValue(cssObjProp) + "<br>";
-//     }
-//     document.getElementById("output").innerHTML = txt;
-// }
-// </script>
-
-
-// Вопрос:
-// Как получить расчетный стиль для фонового цвета у дива с id="test"?
+console.log(clone)
---
+++ mariia_abramova/dz7/app2.js
@@ -0,0 +1,7 @@
+// ------------------------
+// Задача 7.2
+// ------------------------
+// Вопрос:
+// Как получить расчетный стиль для фонового цвета у дива с id="test"?
+
+console.log(window.getComputedStyle(document.getElementById("test")).backgroundColor)
