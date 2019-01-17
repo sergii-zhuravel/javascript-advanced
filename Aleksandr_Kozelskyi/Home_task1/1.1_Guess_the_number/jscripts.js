61fd0f465cee23c1959124608fb884fc6a93b1fd
Alartsp <alartsp@ukr.net>
2019-01-18 00:51:54 +0200

    added all of home tasks 1

--- Aleksandr_Kozelskyi/Home_task1/1.1_Guess_the_number/jscripts.js
+++ Aleksandr_Kozelskyi/Home_task1/1.1_Guess_the_number/jscripts.js
@@ -1,7 +1,7 @@
 var num = prompt("Загадай число от 1 до 10", "");
 var random = Math.ceil(Math.random()*10);
 
-window.onload = function guessNum(num, random) {
+function guessNum(num, random) {
     if (num == random) {
         alert("Угадал");
     } else {
