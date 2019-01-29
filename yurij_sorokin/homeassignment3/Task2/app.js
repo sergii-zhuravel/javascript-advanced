27e19a36735d5bc662b12a977396444ee57e69af
Yurij Sorokin <yurij.sorokin@ua.wappds.com>
2019-01-29 17:23:30 -0500

    home work3

--- yurij_sorokin/homeassignment3/Task2/app.js
+++ yurij_sorokin/homeassignment3/Task2/app.js
@@ -1,9 +1,24 @@
-var myArr = ['0', '4', '3', '2', '1'];
-
-
-function testFunc(arg1) {
-    return arg1.slice().sort();
+function applyAll(fun, context) {
+    var ar = [].slice.call(arguments,1);
+    return fun.apply(context, ar);
 };
 
-console.log(testFunc(myArr));
+console.log(applyAll(Math.max, 2, -2, 3)); 
+console.log(applyAll(Math.min, 2, -2, 3)); 
 
+
+
+function sum() { 
+    return [].reduce.call(arguments, function(a, b) {
+      return a + b;
+    });
+  }
+  
+  function mul() {
+    return [].reduce.call(arguments, function(a, b) {
+      return a * b;
+    });
+  }
+  
+  console.log( applyAll(sum, 1, 2, 3) ); 
+  console.log( applyAll(mul, 2, 3, 4) ); 
