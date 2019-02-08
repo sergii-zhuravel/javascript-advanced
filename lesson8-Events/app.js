3a66c3cbb30a4e6f487436bca1649caad12a98ee
Sergii Zhuravel <sergii.zhuravel@qvantel.com>
2019-02-08 22:06:29 +0200

    updated lesson 8

--- lesson8-Events/app.js
+++ lesson8-Events/app.js
@@ -1,7 +1,30 @@
+ var link = document.querySelector('a');
+
+ link.addEventListener('click', linkClick, true);
+ function linkClick(event) {
+   console.log(event.currentTarget);
+   event.stopPropagation();
+   event.preventDefault();
+  }
+
 var display = document.getElementById('display');
 
 var saveBtn = document.querySelector('[data-action="save"]');
+var loadBtn = document.querySelector('[data-action="load"]');
 
-saveBtn.onClick = function(event) {
-  console.log(event);
+saveBtn.onclick = function(myEvent) {
+  display.innerHTML = "Hello world!" + Date();
 }
+
+function changeText(myEvent) {
+    myEvent.target.innerHTML = "Hello world!";
+    console.log(event.currentTarget);
+    myEvent.stopPropagation();
+}
+function myFunc(){
+  saveBtn.click();
+}
+
+saveBtn.addEventListener('click', changeText);
+loadBtn.addEventListener('click', myFunc);
+// saveBtn.addEventListener('click', changeText, true);
