77fd5775e9c09866f675c7e82113ecdb3fb420e2
Vitaliy <kapron4ik@mail.ru>
2019-02-10 21:41:51 +0200

    Add home work 7,8

---
+++ Vitaliy_Sapronov/Home_work 8/js/my_script1.js
@@ -0,0 +1,12 @@
+// Написать JavaScript код, который:
+// 1. Найдет todo-input и получит значение введенное в поле
+// 2. Найдет элемент li
+// 3. Создаст deep клон элемента li
+// 4. Поменять только текст у клона элемента li
+
+var inputVal = document.getElementById('todo-input').value;
+var liFirst = document.getElementById('todo-list').firstElementChild;
+var liNext = liFirst.cloneNode(true);
+liFirst.parentElement.appendChild(liNext);
+liNext.firstElementChild.innerHTML = inputVal;
+liNext.firstChild.textContent = "Second todo ";
