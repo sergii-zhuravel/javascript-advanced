0a7d8f3a0c5144d8da27961fa9aa3d039de99fba
Yurij Sorokin <yurij.sorokin@ua.wappds.com>
2019-02-24 20:04:10 -0500

    coursework

--- yurij_sorokin/homeassignment9/storage/app.js
+++ yurij_sorokin/homeassignment9/storage/app.js
@@ -1,46 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var logInWindow = document.getElementById("loginWindow");
-    var userExists = document.getElementById("userExists");
-    var loginButton = document.getElementById("login");
-    var logoutButton = document.getElementById("logout");
-    var inputValue = document.getElementById("username");
-   
-    
-  
-    if (inputValue.value) { 
-      inputValue.value = ''; 
-    }
-  
-    if (!localStorage.getItem('userName')) {
-      logInWindow.style.display = "block";
-      userExists.style.display = "none";
-   
-      
-    } else {
-      logInWindow.style.display = "none";
-      userExists.style.display = "block";
-   
-      
-    }
-  
-    function refresh() {
-      location.reload();
-    }
-  
-  
-    loginButton.addEventListener("click", refresh);
-    logoutButton.addEventListener("click", refresh);
-    
-  });
-  
+  var logInWindow = document.getElementById("loginWindow");
+  var userExists = document.getElementById("userExists");
+  var loginButton = document.getElementById("login");
+  var logoutButton = document.getElementById("logout");
+  var inputValue = document.getElementById("username");
 
-    
-    function addRecord() {
-      var login = document.getElementById('username').value;
-        localStorage.userName = login;
-      
-    }
-    
-    function removeRecord() {
-      localStorage.removeItem('userName');
-    }
+  if (inputValue.value) {
+    inputValue.value = "";
+  }
+
+  if (!localStorage.getItem("userName")) {
+    logInWindow.style.display = "block";
+    userExists.style.display = "none";
+  } else {
+    logInWindow.style.display = "none";
+    userExists.style.display = "block";
+  }
+
+  function refresh() {
+    location.reload();
+  }
+
+  loginButton.addEventListener("click", refresh);
+  logoutButton.addEventListener("click", refresh);
+});
+
+function addRecord() {
+  var login = document.getElementById("username").value;
+  localStorage.userName = login;
+}
+
+function removeRecord() {
+  localStorage.removeItem("userName");
+}
